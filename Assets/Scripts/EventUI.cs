2c798eb874d5530ce21bc0ef5beb72a93c4c32ea
fcogomlove <fcogomlove@gmail.com>
2025-02-26 19:46:22 -0600

    Animaciones

--- Assets/Scripts/EventUI.cs
+++ Assets/Scripts/EventUI.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using TMPro;
+using System.Collections.Generic;
 
 public class EventUI : MonoBehaviour
 {
     private int currentIndex = 0;
+
+    public List<string> mensajes;
+    public TextMeshProUGUI texto;
 
     void Start()
     {
@@ -19,6 +24,32 @@ public class EventUI : MonoBehaviour
     public void ChangeScenebyIndex(int index)
     {
         SceneManager.LoadScene(index);
+    }
+
+    public void ReloadCurrentScene()
+    {
+        Scene current = SceneManager.GetActiveScene();
+
+        SceneManager.LoadScene(current.name);
+    }
+
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+
+    public void CycleText()
+    {
+        currentIndex = (currentIndex + 1) % mensajes.Count;
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        if(mensajes.Count > 0 && texto != null)
+        {
+            texto.text = mensajes[currentIndex];
+        }
     }
 
     void Update()
