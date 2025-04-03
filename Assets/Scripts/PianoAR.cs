562f4e8660c01987ff1998d63e659d3748bbae99
fcogomlove <fcogomlove@gmail.com>
2025-04-02 20:02:08 -0600

    Piano AR 2

--- Assets/Scripts/PianoAR.cs
+++ Assets/Scripts/PianoAR.cs
@@ -20,6 +20,89 @@ public class PianoAR : MonoBehaviour
         if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                btnName = hit.transform.name;
+                switch (btnName)
+                {
+                    case "Do":
+                        audioSource.clip = audioClips[0];
+                        audioSource.Play();
+                        break;
+                    case "Re":
+                        audioSource.clip = audioClips[1];
+                        audioSource.Play();
+                        break;
+                    case "Mi":
+                        audioSource.clip = audioClips[2];
+                        audioSource.Play();
+                        break;
+                    case "Fa":
+                        audioSource.clip = audioClips[3];
+                        audioSource.Play();
+                        break;
+                    case "Sol":
+                        audioSource.clip = audioClips[4];
+                        audioSource.Play();
+                        break;
+                    case "La":
+                        audioSource.clip = audioClips[5];
+                        audioSource.Play();
+                        break;
+                    case "Si":
+                        audioSource.clip = audioClips[6];
+                        audioSource.Play();
+                        break;
+                    default:
+                        Debug.Log("Hit en algun otro elemento");
+                        break;
+                }
+            }
+        }else if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                btnName = hit.transform.name;
+                switch (btnName)
+                {
+                    case "Do":
+                        audioSource.clip = audioClips[0];
+                        audioSource.Play();
+                        break;
+                    case "Re":
+                        audioSource.clip = audioClips[1];
+                        audioSource.Play();
+                        break;
+                    case "Mi":
+                        audioSource.clip = audioClips[2];
+                        audioSource.Play();
+                        break;
+                    case "Fa":
+                        audioSource.clip = audioClips[3];
+                        audioSource.Play();
+                        break;
+                    case "Sol":
+                        audioSource.clip = audioClips[4];
+                        audioSource.Play();
+                        break;
+                    case "La":
+                        audioSource.clip = audioClips[5];
+                        audioSource.Play();
+                        break;
+                    case "Si":
+                        audioSource.clip = audioClips[6];
+                        audioSource.Play();
+                        break;
+                    default:
+                        Debug.Log("Hit en algun otro elemento");
+                        break;
+                }
+            }
         }
     }
 }
