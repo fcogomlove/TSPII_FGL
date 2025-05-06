265866f352f7a1a93b48920faa5c7bc99b9a3a87
fcogomlove <fcogomlove@gmail.com>
2025-05-05 19:53:26 -0600

    BD Cartas

---
+++ Assets/Scripts/BDYugiOh.cs
@@ -0,0 +1,190 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Firebase.Database;
+using Firebase.Extensions;
+using Newtonsoft.Json;
+using TMPro;
+
+public class BDYugiOh : MonoBehaviour
+{
+    public DatabaseReference reference;
+
+    [SerializeField]
+    TMP_InputField cardNameInput;
+    [SerializeField]
+    TMP_InputField attackInput;
+    [SerializeField]
+    TMP_InputField defenseInput;
+    [SerializeField]
+    TMP_InputField spellTrapInput;
+    [SerializeField]
+    Toggle isMonsterToggle;
+    [SerializeField]
+    BDYugiOh yugiOhCards;
+
+    public bool isMonster = true;
+    public string cardName;
+    public int attack;
+    public int defense;
+    public string spellTrap;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    public void Start()
+    {
+        cardNameInput.text = "";
+        attackInput.text = "";
+        defenseInput.text = "";
+        spellTrapInput.text = "";
+        isMonster = true;
+        isMonsterToggle.isOn = true;
+
+        spellTrapInput.gameObject.SetActive(false);
+
+        attackInput.gameObject.SetActive(true);
+        defenseInput.gameObject.SetActive(true);
+    }
+
+    public void Booleano(bool toogleB)
+    {
+        isMonster = toogleB;
+    }
+
+    public void Registro()
+    {
+        //Primer tipo de dato
+        //Generar una clave única para el registro
+        string keyNombre = reference.Child("Nombre").Push().Key;
+        //reference.Child("Nombre").Child(keyNombre).SetValueAsync(textoNombre.text);
+
+        //Clave unica para datos individuales
+        string keyDatos = reference.Child("Datos").Push().Key;
+
+
+        //Segundo tipo de dato que es un valor individual string
+        reference.Child("Datos").Child(keyDatos).Child("Texto").SetValueAsync("Registro de texto");
+
+        //Tercer tipo de dato que es un valor int
+        //reference.Child("Datos").Child(keyDatos).Child("Edad").SetValueAsync(int.Parse(textoEdad.text));
+
+        //Cuarto tipo de dato que es un bool
+        //reference.Child("Datos").Child(keyDatos).Child("Registro").SetValueAsync(registroBooleano);
+
+        //Datos a actualizar
+        Debug.Log("Dato Escuela actualizado: UNAM");
+        reference.Child("Escuela").SetValueAsync("UNAM");
+        Debug.Log("Dato Año actualizado: 2025");
+        reference.Child("Año").SetValueAsync(2025);
+
+        Debug.Log("Dato Escuela actualizado: TEC");
+        reference.Child("Escuela").SetValueAsync("TEC");
+        Debug.Log("Dato Año actualizado: 2026");
+        reference.Child("Año").SetValueAsync(2026);
+
+        //Registro de objeto tipo Usuario
+        Usuario usuarioN = new Usuario("Jacobo", "jac@gmail.com");
+
+        string json = JsonUtility.ToJson(usuarioN);
+        reference.Child("Usuario1").SetRawJsonValueAsync(json);
+
+
+    }
+
+    public void LoadFromBD()
+    {
+
+        //Carga de valor individual
+        reference.Child("Año").GetValueAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Error al obtener el valor de la base de datos: " + task.Exception);
+            }
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                if (snapshot.Exists)
+                {
+                    string value = snapshot.Value.ToString();
+                    Debug.Log("Tipo de valor obtenido: " + snapshot.Value.GetType());
+                    Debug.Log(value);
+                }
+            }
+            else
+            {
+                Debug.Log("Registro no encontrado");
+            }
+        });
+
+        //Carga de valores anidados
+        reference.Child("Nombre").GetValueAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Error al obtener el valor de la base de datos: " + task.Exception);
+            }
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot2 = task.Result;
+                if (snapshot2.Exists)
+                {
+                    //Recorrer todos los hijos de Nombre y obtener los valores
+
+                    foreach (DataSnapshot childSnapshot in snapshot2.Children)
+                    {
+                        string value2 = childSnapshot.Value.ToString();
+                        Debug.Log("Tipo de valor obtenido: " + childSnapshot.Value.GetType());
+                        Debug.Log(value2);
+                    }
+
+                }
+            }
+            else
+            {
+                Debug.Log("Registro no encontrado");
+            }
+        });
+
+        //Carga de valores tipo JSON
+        reference.Child("Usuario1").GetValueAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Error al obtener el valor de la base de datos: " + task.Exception);
+            }
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot3 = task.Result;
+                if (snapshot3.Exists)
+                {
+                    //Convertir el JSON a un diccionario
+
+                    Dictionary<string, object> userData = JsonConvert.DeserializeObject<Dictionary<string, object>>(snapshot3.GetRawJsonValue());
+                    Debug.Log(userData.GetType());
+                    string nombre = (string)userData["UserName"];
+                    string email = (string)userData["Email"];
+
+                    Debug.Log($"Nombre de usuario: {nombre}, Correo: {email}");
+
+                }
+            }
+            else
+            {
+                Debug.Log("Registro no encontrado");
+            }
+        });
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
